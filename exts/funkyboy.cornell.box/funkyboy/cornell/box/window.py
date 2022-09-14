4fa97986d25e72fcce2586f61a35ac5d6b610264
Stephen Tong/Funky Boy <stephen.tong@mail.utoronto.ca>
2022-09-13 21:00:32 -0400

    Color Widgets working

--- exts/funkyboy.cornell.box/funkyboy/cornell/box/window.py
+++ exts/funkyboy.cornell.box/funkyboy/cornell/box/window.py
@@ -1,11 +1,9 @@
-
-#from .box_maker_2 import BoxMaker2
-from .box_maker import BoxMaker
 import omni.ext
 import omni.ui as ui
 import omni.kit.commands
-from pxr import Gf
 import omni.usd
+from pxr import Gf, Sdf
+from .box_maker import BoxMaker
 
 WINDOW_TITLE = "Cornell Box Maker"
 
@@ -16,19 +14,39 @@ class CornellBoxWindow(ui.Window):
         super().__init__(title, width=450, height=300)
         self._menu_path = menu_path
         self.set_visibility_changed_fn(self._on_visibility_changed)
-        self._build_ui()
-    
-    def on_shutdown(self):
-        self._win = None
+        
 
-    def show(self):
-        self.visible = True
-        self.focus()    
-    
-    def hide(self):
-        self.visible = False
+#color widget variables
+        self._color_model = None
+        self._color_model_2 = None
+        self._color_changed_subs = []
+        self._color_changed_subs_2 = []
+        self._path_model = None 
+        self._path_model_2 = None
+        self._change_info_path_subscription = None
+        self._change_info_path_subscription_2 = None
+        self._stage = omni.usd.get_context().get_stage()
+        self._OmniPBR_Path_03 = "/World/Looks/OmniPBR_03/Shader.inputs:diffuse_color_constant"
+        self._OmniPBR_Path_04 =  "/World/Looks/OmniPBR_04/Shader.inputs:diffuse_color_constant" 
+        self.frame.set_build_fn(self._build_window)
 
-    def _build_ui(self):
+#subscribe mat 1
+        attr_path = self._OmniPBR_Path_03
+        color_attr = self._stage.GetAttributeAtPath(attr_path)
+        if color_attr:
+            self._change_info_path_subscription = omni.usd.get_watcher().subscribe_to_change_info_path(
+                     attr_path, 
+                     self._on_mtl_attr_changed)
+
+
+#subscribe mat 2
+        attr_path_2 = self._OmniPBR_Path_04 
+        color_attr_2 = self._stage.GetAttributeAtPath(attr_path_2)
+        if color_attr_2:
+            self._change_info_path_subscription_2 = omni.usd.get_watcher().subscribe_to_change_info_path(
+                    attr_path_2, self._on_mtl_attr_changed_2)        
+
+    def _build_window(self):
         
         with self.frame:
             
@@ -41,25 +59,32 @@ class CornellBoxWindow(ui.Window):
                         ui.Button("Make Box", clicked_fn=lambda: on_click()) 
                                     
                                     
-                with ui.CollapsableFrame("Color *not working yet...*", name="group"):
+                with ui.CollapsableFrame("Color", name="group"):
                     with ui.VStack(height=0, spacing=SPACING):           
                         with ui.HStack(height=0, spacing=SPACING):
-                                ui.Label("color wall 1: ", height=0, width=0)
-                                color_model = ui.ColorWidget(0.9, 0.0, 0.0, height=0).model
-                                for item in color_model.get_item_children():
-                                    component = color_model.get_item_value_model(item)
+                                ui.Label("colored wall 1: ", height=0, width=0)
+                                self._color_model = ui.ColorWidget(0.9, 0.0, 0.0, height=0).model
+                                for item in self._color_model.get_item_children():
+                                    component = self._color_model.get_item_value_model(item)
+                                    self._color_changed_subs.append(component.subscribe_value_changed_fn(self._on_color_changed))
                                     #ui.FloatDrag(component)
+                                                
+                        self._path_model = self._OmniPBR_Path_03                        
                         
                         with ui.HStack(height=0, spacing=SPACING):
-                                ui.Label("color wall 2: ", height=0, width=0)  
-                                color_model2 = ui.ColorWidget(0.0, 0.9, 0.2, height=0).model
-                                for item in color_model2.get_item_children():
-                                    component = color_model2.get_item_value_model(item)
+                                ui.Label("colored wall 2: ", height=0, width=0)  
+                                self._color_model_2 = ui.ColorWidget(0.0, 0.0, 0.9, height=0).model
+                                for item in self._color_model_2.get_item_children():
+                                    component = self._color_model_2.get_item_value_model(item)
+                                    self._color_changed_subs_2.append(component.subscribe_value_changed_fn(self._on_color_changed_2))
                                     #ui.FloatDrag(component)
-                        with ui.HStack(height=0, spacing=SPACING):
-                                ui.Label("Select preset style:",  height=0, width=0)
-                                ui.ComboBox(0, "Classic", "SIGGRAPH 1984", "NVIDIA Omniverse", height=0)
-                                ui.Spacer(width=1)
+
+                        self._path_model_2 = self._OmniPBR_Path_04            
+#disabled checkbox group
+                        # with ui.HStack(height=0, spacing=SPACING):
+                        #         ui.Label("Select preset style:",  height=0, width=0)
+                        #         ui.ComboBox(0, "Classic", "SIGGRAPH 1984", "NVIDIA Omniverse", height=0)
+                        #         ui.Spacer(width=1)
 
                 ui.Spacer(width=0,height=0)
                 self._slider_model_x = ui.SimpleFloatModel()
@@ -146,8 +171,70 @@ class CornellBoxWindow(ui.Window):
                                 p=self._source_prim_model: update_scale_z(p, m.as_float))
 
 
+    def _on_mtl_attr_changed(self, path):
+        color_attr = self._stage.GetAttributeAtPath(path)
+        color_model_items = self._color_model.get_item_children()
+        if color_attr:     
+            color = color_attr.Get()
+            for i in range(len(color)):
+                component = self._color_model.get_item_value_model(color_model_items[i])
+                component.set_value(color[i])
+
+    def _on_mtl_attr_changed_2(self, path):
+        color_attr = self._stage.GetAttributeAtPath(path)
+        color_model_items = self._color_model_2.get_item_children()
+        if color_attr:
+            color = color_attr.Get()
+            for i in range(len(color)):
+                component = self._color_model_2.get_item_value_model(color_model_items[i])
+                component.set_value(color[i])
+
+
+
+    def _on_color_changed(self, model):
+        values = []
+        for item in self._color_model.get_item_children():
+            component = self._color_model.get_item_value_model(item)
+            values.append(component.as_float)
+
+        if Sdf.Path.IsValidPathString(self._path_model):
+            attr_path = Sdf.Path(self._path_model)
+            color_attr = self._stage.GetAttributeAtPath(attr_path)
+            if color_attr:
+                color_attr.Set(Gf.Vec3f(*values[0:3]))
+
+
+    def _on_color_changed_2(self, model):
+        values = []
+        for item in self._color_model_2.get_item_children():
+            component = self._color_model_2.get_item_value_model(item)
+            values.append(component.as_float)
+
+
+        if Sdf.Path.IsValidPathString(self._path_model_2):
+            attr_path = Sdf.Path(self._path_model_2)
+            color_attr = self._stage.GetAttributeAtPath(attr_path)
+            if color_attr:
+                color_attr.Set(Gf.Vec3f(*values[0:3]))
 
 
 
     def _on_visibility_changed(self, visible):
         omni.kit.ui.get_editor_menu().set_value(self._menu_path, visible)
+
+
+    def destroy(self) -> None:
+        self._change_info_path_subscription = None
+        self._color_changed_subs = None
+        self._path_changed_sub = None
+        return super().destroy()
+
+    def on_shutdown(self):
+        self._win = None
+
+    def show(self):
+        self.visible = True
+        self.focus()    
+    
+    def hide(self):
+        self.visible = False
